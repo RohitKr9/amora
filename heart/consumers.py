3ffdc1e520cabe0d4b0530dbbe071e4527dfb5f9
RohitKr9 <rohitpoddar937@gmail.com>
2025-04-08 00:51:08 +0530

    Add channel layer

--- heart/consumers.py
+++ heart/consumers.py
@@ -1,20 +1,37 @@
 from channels.generic.websocket import WebsocketConsumer
 from channels.exceptions import DenyConnection
+from asgiref.sync import async_to_sync
+import json
 
 class ChatConsumer(WebsocketConsumer):
     
     def connect(self):
         user = self.scope['user'] #it contains user object
-        user_id = user.id
 
         if user.is_anonymous:
             raise DenyConnection() #This is not mentioned in documentation (Way to close connection )
         
+        user_id = user.id
+        self.room_name = f"room_{user_id}"
+        self.room_group_name = self.room_name
+        async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name) #all channel layer methods are async
         self.accept()
 
     def disconnect(self, closing_code):
-        
         print(f'There is something wrong -> Code = {closing_code}')
 
     def receive(self, text_data):
-        pass
+        text_data_loaded = json.loads(text_data)
+        message = text_data_loaded['message']
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                "type":"chat_message",
+                "message" : message
+            }
+        )
+    
+    def chat_message(self, event):
+        message = event["message"]
+        self.send(text_data = json.dumps({"message":message}))
